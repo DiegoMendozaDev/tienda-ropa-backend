bd1cb301c555b3b3aca22cba1ebbb19a03a0024f
ATICMCH <proyectosatic@mycityhome.es>
2025-06-07 15:32:29 +0200

    genero

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -35,6 +35,7 @@ class ProductoController extends AbstractController
             'id_categoria' => $producto->getCategoria()->getId(),
             'foto' => $producto->getFoto(),
             'stock' => $producto->getStock(),
+            'genero' => $producto->getGenero(),
             'unidades_vendidas' => $producto->getUnidades_vendidas()
         ];
     }
